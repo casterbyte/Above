6dcde09b6721ff4e9df1e8660c6bcd3d55444148
Caster <88490676+wearecaster@users.noreply.github.com>
2023-11-30 21:03:50 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/wearecaster/above",
     author="Caster",
     author_email="casterinfosec@gmail.com",
-    scripts=['Above.py'],
+    scripts=['above.py'],
     description="Autonomous network sniffer for finding network vulnerabilities",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -18,7 +18,7 @@ setup(
         'colorama',
     ],
 entry_points={
-    "console_scripts": ["above = Above:main"],
+    "console_scripts": ["above = above:main"],
     },
 
     python_requires='>=3.11',
