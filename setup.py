e426ac35a4baa445d45ee68b7d47371ae05d3806
Caster <88490676+wearecaster@users.noreply.github.com>
2023-11-30 16:45:13 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="above",
+    version="2.1",
+    url="https://github.com/wearecaster/above",
+    author="Caster",
+    author_email="casterinfosec@gmail.com",
+    scripts=['Above.py'],
+    description="Autonomous network sniffer for finding network vulnerabilities",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="Apache-2.0",
+    keywords=['information gathering', 'penetration testing', 'network security', 'network sniffer'],
+    packages=find_packages(),
+    install_requires=[
+        'scapy',
+        'colorama',
+    ],
+entry_points={
+    "console_scripts": ["above = Above:main"],
+    },
+
+    python_requires='>=3.11',
+)
